79b8c7dc671b9bf573f620b53e2f206848b5bacd
John <bioj@hotmail.fr>
2022-07-05 18:22:02 +0900

    GW3S diamond: level5

--- contracts/GW3S/level/facets/Level5Facet.sol
+++ contracts/GW3S/level/facets/Level5Facet.sol
@@ -2,31 +2,48 @@
 
 pragma solidity ^0.8.1;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import {AppStorage, LibAppStorage} from "../../libraries/LibAppStorage.sol";
 import {Modifiers} from "../../libraries/LibLevel.sol";
+
+contract Token is ERC20, Ownable {
+    constructor (string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount * 10 ** decimals());
+    }
+}
 
 contract Level5Facet is Modifiers {
 
     event ClaimReward(uint256 indexed level, address indexed player);
     event Completed(uint256 indexed level, address indexed player);
+    event DeployedInstance(uint256 indexed level, address indexed player, address instance);
 
+    function initLevel5() external returns(address) {
+        Token token = new Token("Who am I ?", "CATCH");
+        token.mint(msg.sender, 1);
+        
+        s.level_completed[msg.sender][5] = false;
+        s.level_running[msg.sender] = 5;
+        s.level_instance[msg.sender][5] = address(this);
 
-    function complete_l1() external hasCompleted(1) isRunning(1) {
-        s.level_completed[msg.sender][1] = true;
+        emit DeployedInstance(5, msg.sender, address(this));
+        return address(this);
+    }
+
+    function complete_l5(address who) external hasCompleted(5) isRunning(5) {
+        require(who == address(this), "Wrong address !");
+        s.level_completed[msg.sender][5] = true;
         emit Completed(0, msg.sender);
     }
     
     /// @notice Claim reward.
-    function claim_l1() external hasClaimed(1) {
-        require(s.level_completed[msg.sender][1] == true, "Claim_l1: You need to complete the level first");
-        s.level_reward[msg.sender][1] = true;
+    function claim_l5() external hasClaimed(5) {
+        require(s.level_completed[msg.sender][5] == true, "Claim_l5: You need to complete the level first");
+        s.level_reward[msg.sender][5] = true;
         emit ClaimReward(0, msg.sender);
     }
 
-    /// @notice Claim reward.
-    function claim_l3() external hasClaimed(1) {
-        require(s.level_completed[msg.sender][1] == true, "Claim_l1: You need to complete the level first");
-        s.level_reward[msg.sender][1] = true;
-        emit ClaimReward(0, msg.sender);
-    }
 }
---
+++ contracts/GW3S/level/upgradeInitializers/InitLevel5.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+/******************************************************************************\
+* Author: Nick Mudge <nick@perfectabstractions.com> (https://twitter.com/mudgen)
+* EIP-2535 Diamonds: https://eips.ethereum.org/EIPS/eip-2535
+*
+* Implementation of a diamond.
+/******************************************************************************/
+
+import {LibAppStorage, AppStorage} from "../../libraries/LibAppStorage.sol";
+import {Level, Difficulty} from "../../libraries/LibLevel.sol";
+
+// It is expected that this contract is customized if you want to deploy your diamond
+// with data from a deployment script. Use the init function to initialize state variables
+// of your diamond. Add parameters to the init funciton if you need to.
+
+contract InitLevel5 {    
+
+    AppStorage internal s;
+
+    function init(address addr_) external {
+        s.level[5].addr = addr_;
+        s.level[5].id = 5;
+        s.level[5].title = "Catch me if you can";
+        s.level[5].difficulty = Difficulty.EASY;
+        // More info here: https://eips.ethereum.org/EIPS/eip-2535#diamond-interface 
+    }
+}
