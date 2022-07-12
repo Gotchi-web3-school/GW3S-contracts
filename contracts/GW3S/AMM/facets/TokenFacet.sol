dd03fca2db4b58e2fdde83d0054bd38e4fa0e465
John <bioj@hotmail.fr>
2022-07-12 19:08:48 +0900

    GW3S diamond: TokenFacet

---
+++ contracts/GW3S/AMM/facets/TokenFacet.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract Token is ERC20, Ownable {
+    constructor (string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount * 10 ** decimals());
+    }
+}
+
+contract TokenFacet {
+
+    event Deployed(string indexed name, string indexed ticker, uint256 indexed supply);
+
+    function deployToken(string memory name, string memory ticker, uint256 totalSupply) public returns(address) {
+        Token token = new Token(name, ticker);
+        token.mint(msg.sender, totalSupply);
+        emit Deployed(name, ticker, totalSupply);
+
+        return address(token);
+    }
+}
