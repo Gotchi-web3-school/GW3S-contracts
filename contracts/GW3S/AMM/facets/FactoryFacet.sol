52c98c9b2ab4b011c59f1eb8d6ff654023112231
John <bioj@hotmail.fr>
2022-07-12 18:48:28 +0900

    fix FacetoryFacet

--- contracts/GW3S/AMM/facets/FactoryFacet.sol
+++ contracts/GW3S/AMM/facets/FactoryFacet.sol
@@ -2,14 +2,11 @@
 pragma solidity ^0.8.15;
 
 import '../../../uniswap/v2-core/contracts/UniswapV2Factory.sol';
-import {AppStorage} from "../../libraries/LibAppStorage.sol";
 
 contract FactoryFacet {
-    AppStorage internal s;
 
     function deployFactory(address player) external returns(address) {
         UniswapV2Factory factory = new UniswapV2Factory(player);
-        s.factory[player] = address(factory);
         return address(factory);
     }
 }
