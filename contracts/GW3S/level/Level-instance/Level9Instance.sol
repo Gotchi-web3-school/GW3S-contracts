496436e7f34d24ee5217ee912b5410332f874dc2
John <bioj@hotmail.fr>
2022-07-13 15:07:26 +0900

    GW3S diamond: level 9

--- contracts/GW3S/level/Level-instance/Level9Instance.sol
+++ contracts/GW3S/level/Level-instance/Level9Instance.sol
@@ -2,8 +2,7 @@
 
 pragma solidity ^0.8.15;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import "../../AMM/facets/TokenFacet.sol";
 import "../../AMM/interfaces/IFactory.sol";
 import "../../AMM/interfaces/IRouter.sol";
 import '../../../uniswap/v2-core/contracts/libraries/UniswapV2Library.sol';
@@ -11,25 +10,17 @@ import '../../../uniswap/v2-core/contracts/libraries/UniswapV2Library.sol';
 address constant WETH = 0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa;
 uint256 constant MAX = 25;
 
-contract Token is ERC20, Ownable {
-    constructor (string memory name, string memory symbol) ERC20(name, symbol) {}
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount * 10 ** decimals());
-    }
-}
-
 contract Level9Instance {
     address[2] public tokens;
+    address public factory;
     address public player;
+    address public diamond;
     string[] public TOKENS_NAME = ["level9 USDC"];
     string[] public TOKENS_SYMBOL = ["USDC"];
-    address factory;
-
-    event Deployed(string indexed name, string indexed ticker, uint256 indexed supply);
 
     constructor(address player_) {
         player = player_;
+        diamond = msg.sender;
 
         tokens[0] = address(new Token(TOKENS_NAME[0], TOKENS_SYMBOL[0]));
         Token(tokens[0]).mint(player_, MAX);
@@ -52,10 +43,6 @@ contract Level9Instance {
     }
 
     function deployToken(string memory name, string memory ticker, uint256 totalSupply) public {
-        Token token = new Token(name, ticker);
-        token.mint(msg.sender, totalSupply);
-        tokens[1] = address(token);
-
-        emit Deployed(name, ticker, totalSupply);
+        tokens[1] = TokenFacet(diamond).deployToken(name, ticker, totalSupply, msg.sender);
     }
 }
