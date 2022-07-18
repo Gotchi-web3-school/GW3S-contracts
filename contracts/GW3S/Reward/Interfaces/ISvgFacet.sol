0fbd30d0a6111467e78133343d5009a618445f21
John <bioj@hotmail.fr>
2022-07-18 22:01:44 +0900

    GW3S contracts: Interface SvgFacet

--- contracts/GW3S/Reward/Interfaces/ISvgFacet.sol
+++ contracts/GW3S/Reward/Interfaces/ISvgFacet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15; 
  
-interface ISvg {
+interface ISvgFacet {
     function getLevelRewardSvg(uint256 _levelId, uint256 _type) external view returns (string memory ag_);
 
     function storeSvg(string calldata _svg, uint256 levelId, uint256 _type) external;
