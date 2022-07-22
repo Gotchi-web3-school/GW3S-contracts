c3a292c98fefa6b708de1fb39a49392ee4790af0
John <bioj@hotmail.fr>
2022-07-22 19:23:44 +0900

    GW3S contracts:  LevelLoupe interface updted

--- contracts/GW3S/level/interfaces/ILevelLoupeFacet.sol
+++ contracts/GW3S/level/interfaces/ILevelLoupeFacet.sol
@@ -3,8 +3,12 @@
 pragma solidity 0.8.15;
 
 interface ILevelLoupeFacet {
+    function getAddress(uint256 levelId) external view returns (address addr);
     function getTitle(uint256 levelId) external view returns (string memory title);
     function getLevelDifficulty(uint256 levelId) external view returns (string memory title);
     function hasCompletedLevel(address account, uint256 levelId) external view returns (bool result);
     function hasClaimedLevel(address account, uint256 levelId) external view returns (bool result);
+    function getRunningLevel(address account) external view returns (uint256 result);
+    function getLevelInstanceByAddress(address account, uint256 levelId) external view returns (address result);
+    function getRouter() external view returns (address result);
 }
