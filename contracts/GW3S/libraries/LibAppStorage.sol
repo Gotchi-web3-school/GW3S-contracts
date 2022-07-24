cd8c163615f6258f801193767d4e30af1a06b938
John <bioj@hotmail.fr>
2022-07-25 03:05:22 +0900

    GW3S contracts: add level_factory storage

--- contracts/GW3S/libraries/LibAppStorage.sol
+++ contracts/GW3S/libraries/LibAppStorage.sol
@@ -6,19 +6,17 @@ import {LibDiamond} from "../../shared/libraries/LibDiamond.sol";
 import {Level} from "./LibLevel.sol";
 
 struct AppStorage {
-    // levels (level) state variable
-    mapping(uint256 => Level) level;                                    // Store Level by his id  
-    mapping(address => mapping(uint256 => bool)) level_completed;       // Store level completed by player and Id of the level
-    mapping(address => mapping(uint256 => bool)) level_reward;          // Store level reward by address and Id of the level; return true if claimed
-    mapping(address => mapping(uint256 => bool)) secret_reward;         // Store level reward by address and Id of the level; return true if claimed
-    mapping(address => mapping(uint256 => bool)) hacker_reward;         // Store level reward by address and Id of the level; return true if claimed
-    mapping(address => uint256) level_running;                          // The current level that player try to solve
-    mapping(address => mapping(uint256 => address)) level_instance;     // The current instance of player deployed by the a specific level 
+    // levels state variable
+    mapping(uint256 => Level) level;                                     // Store Level by his id  
+    mapping(address => mapping(uint256 => bool)) level_completed;        // Store level completed by player and Id of the level
+    mapping(address => mapping(uint256 => bool)) level_reward;           // Store level reward by address and Id of the level; return true if claimed
+    mapping(address => mapping(uint256 => bool)) secret_reward;          // Store level reward by address and Id of the level; return true if claimed
+    mapping(address => mapping(uint256 => bool)) hacker_reward;          // Store level reward by address and Id of the level; return true if claimed
+    mapping(address => uint256) level_running;                           // The current level that player try to solve
+    mapping(address => mapping(uint256 => address)) level_instance;      // The current instance of player deployed by the a specific level 
     // type = enum{LEVEL, HIDDEN, HACKER}
-    mapping(uint => mapping(uint => address)) Erc721LevelReward;            // Store ERC721 smart contract by (levelId => type => svgContract)
-
-    // AMM state
-    address router;                                 // The address of the router facet
+    mapping(uint => mapping(uint => address)) Erc721LevelReward;         // Store ERC721 smart contract by (levelId => type => svgContract)
+    mapping(uint256 => address) level_factory;                           // The address of factory by level
 }
 
 struct SvgStorage {
