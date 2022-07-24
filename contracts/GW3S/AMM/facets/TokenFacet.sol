5e984cc4c7e1334094e1f9348fdd954024d562c3
John <bioj@hotmail.fr>
2022-07-24 18:49:37 +0900

    GW3S contracts: Added Tokenfacet

--- contracts/GW3S/AMM/facets/TokenFacet.sol
+++ contracts/GW3S/AMM/facets/TokenFacet.sol
@@ -5,7 +5,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract Token is ERC20, Ownable {
-    constructor (string memory name, string memory symbol) ERC20(name, symbol) {}
+    constructor (string memory name, string memory symbol, address owner) ERC20(name, symbol) {
+        transferOwnership(owner);
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount * 10 ** decimals());
@@ -13,14 +15,10 @@ contract Token is ERC20, Ownable {
 }
 
 contract TokenFacet {
-
     event Deployed(string indexed name, string indexed ticker, uint256 indexed supply);
 
-    function deployToken(string memory name, string memory ticker, uint256 totalSupply, address player) public returns(address) {
-        Token token = new Token(name, ticker);
-        token.mint(player, totalSupply);
-        emit Deployed(name, ticker, totalSupply);
-
+    function deployToken(string memory name, string memory ticker) public returns(address) {
+        Token token = new Token(name, ticker, msg.sender);
         return address(token);
     }
 }
