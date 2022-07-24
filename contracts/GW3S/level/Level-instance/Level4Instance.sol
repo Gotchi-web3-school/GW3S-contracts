42c2bc834f8cccafa40b312fcb61b43e26d15c11
John <bioj@hotmail.fr>
2022-07-25 03:06:04 +0900

    GW3S contracts: Reducing gas for level4 instance

--- contracts/GW3S/level/Level-instance/Level4Instance.sol
+++ contracts/GW3S/level/Level-instance/Level4Instance.sol
@@ -2,60 +2,47 @@
 
 pragma solidity ^0.8.15;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import "../../AMM/interfaces/IToken.sol";
 import "../../AMM/interfaces/IRouter.sol";
 import "../../AMM/interfaces/IFactory.sol";
 import "../../AMM/facets/FactoryFacet.sol";
-import '../../../uniswap/v2-core/contracts/libraries/UniswapV2Library.sol';
 
 uint constant MAX = 10000000 * 10 ** 18;
-address constant WETH = 0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa;
-
-contract Token is ERC20, Ownable {
-    constructor (string memory name, string memory symbol) ERC20(name, symbol) {}
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount * 10 ** decimals());
-    }
-}
 
 contract Level4Instance {
     address public player;
-    address[7] tokens;
-    address public factory;
-    string[] public TOKENS_NAME = ["level4 GHST", "level4 ETH", "level4 AAVE", "level4 DAI"];
-    string[] public TOKENS_SYMBOL = ["GHST", "ETH", "AAVE", "DAI"];
+    address[4] public tokens;
 
-    constructor(address player_, address router) {
+    constructor(address player_, address factory) {
+        string[4] memory TOKENS_NAME = ["level4 GHST", "level4 ETH", "level4 AAVE", "level4 DAI"];
+        string[4] memory TOKENS_SYMBOL = ["GHST", "ETH", "AAVE", "DAI"];
         player = player_;
-        for (uint8 i = 0; i < 4; i++) {
-            tokens[i] = address(new Token(TOKENS_NAME[i], TOKENS_SYMBOL[i]));
-            Token(tokens[i]).mint(address(this), 10000000);
-            Token(tokens[i]).approve(router, MAX);
-        }
-        Token(tokens[0]).mint(player_, 10);
 
-        factory = FactoryFacet(msg.sender).deployFactory(player);
-        IRouter(router).addLiquidity(tokens[0], tokens[1], MAX, MAX, MAX, MAX, address(this), block.timestamp, factory);
-        IRouter(router).addLiquidity(tokens[2], tokens[1], MAX, MAX, MAX, MAX, address(this), block.timestamp, factory);
-        IRouter(router).addLiquidity(tokens[4], tokens[2], MAX, MAX, MAX, MAX, address(this), block.timestamp, factory);
+        for (uint8 i = 0; i < 4; i++) {
+            tokens[i] = IToken(msg.sender).deployToken(TOKENS_NAME[i], TOKENS_SYMBOL[i]);
+            IToken(tokens[i]).mint(address(this), 20000000);
+            IToken(tokens[i]).approve(msg.sender, MAX * 2);
+        }
+
+        IToken(tokens[3]).mint(player_, 10);
+
+        IRouter(msg.sender).addLiquidity(tokens[0], tokens[1], MAX, MAX, MAX, MAX, address(this), block.timestamp, factory);
+        IRouter(msg.sender).addLiquidity(tokens[2], tokens[1], MAX, MAX, MAX, MAX, address(this), block.timestamp, factory);
+        IRouter(msg.sender).addLiquidity(tokens[3], tokens[2], MAX, MAX, MAX, MAX, address(this), block.timestamp, factory);
     }
 
-    function getPair(address token0, address token1) public returns(address pair) {
+    function getPair(address token0, address token1, address factory) public returns(address pair) {
         bytes32 tmp;
-        token0 = tokens[0] < tokens[1] ? tokens[0] : tokens[1];
-        token1 = tokens[0] > tokens[1] ? tokens[0] : tokens[1];
+        address tokenA = token0 < token1 ? token0 : token1;
+        address tokenB = token0 > token1 ? token0 : token1;
 
         tmp = keccak256(abi.encodePacked(
             hex'ff',
             factory,
-            keccak256(abi.encodePacked(token0, token1)),
+            keccak256(abi.encodePacked(tokenA, tokenB)),
             IFactory(factory).INIT_CODE_HASH()
         ));
 
         pair = address(uint160(uint256(tmp)));
     }
-
-    
 } 
--- contracts/GW3S/level/Level-instance/interfaces/ILevel4Instance.sol
+++ contracts/GW3S/level/Level-instance/interfaces/ILevel4Instance.sol
@@ -5,6 +5,5 @@ pragma solidity ^0.8.15;
 interface ILevel4Instance {
     function player() external view returns (address); 
     function tokens(uint) external view returns (address);
-    function TOKENS_SYMBOL(uint) external view returns (string memory);
-    function TOKENS_NAME(uint) external view returns (string memory);
+    function getPair(address token0, address token1, address factory) external returns(address pair);
 }
