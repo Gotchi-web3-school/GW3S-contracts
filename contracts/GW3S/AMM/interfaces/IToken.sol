01e0ac9dba9a4797cf95ce33a2179fb096a00434
John <bioj@hotmail.fr>
2022-07-27 18:24:09 +0900

    GW3S contracts: adding transferOwnership

--- contracts/GW3S/AMM/interfaces/IToken.sol
+++ contracts/GW3S/AMM/interfaces/IToken.sol
@@ -15,4 +15,6 @@ interface IToken {
     function approve(address spender, uint value) external returns (bool);
     function transfer(address to, uint value) external returns (bool);
     function transferFrom(address from, address to, uint value) external returns (bool);
+
+    function transferOwnership(address newOwner) external;
 }
