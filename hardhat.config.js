f83bc86b55b0dbc7ba6cff3b58fa84298f25e02d
John <bioj@hotmail.fr>
2022-07-27 19:01:17 +0900

    GW3S hardhat config: Adding linter plugin

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@
 /* global ethers task */
 require('@nomiclabs/hardhat-waffle')
 require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-solhint");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
