1332a22b858d23ef09f52331ab94d3497f4b8e3f
John <bioj@hotmail.fr>
2022-07-29 00:22:23 +0900

    GW3S contracts: fixing level 11

--- contracts/GW3S/level/Level-instance/Level11Instance.sol
+++ contracts/GW3S/level/Level-instance/Level11Instance.sol
@@ -2,24 +2,15 @@
 
 pragma solidity ^0.8.15;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import "../../../uniswap/v2-core/contracts/libraries/SafeMath.sol";
 import "../../AMM/interfaces/IFactory.sol";
 import "../../AMM/interfaces/IRouter.sol";
 import "../../AMM/interfaces/IPair.sol";
-import '../../../uniswap/v2-core/contracts/libraries/UniswapV2Library.sol';
+import "../../AMM/interfaces/IToken.sol";
+import "../../AMM/facets/TokenFacet.sol";
+import "../../../uniswap/v2-core/contracts/libraries/UniswapV2Library.sol";
 
-address constant WETH = 0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa;
-uint256 constant MAX = 1000000000;
-
-contract Token is ERC20, Ownable {
-    constructor (string memory name, string memory symbol) ERC20(name, symbol) {}
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount * 10 ** decimals());
-    }
-}
+uint256 constant MAX = 10000000000;
 
 contract Level11Instance {
     using SafeMath for uint256;
@@ -29,22 +20,22 @@ contract Level11Instance {
     bool public success;
     string[] public TOKENS_NAME = ["level11 DAI", "level11 GHST"];
     string[] public TOKENS_SYMBOL = ["DAI", "GHST"];
-    address factory;
-    address router;
+    address public factory;
+    address public diamond;
 
     event Deployed(string indexed name, string indexed ticker, uint256 indexed supply);
 
-    constructor(address player_, address router_) {
+    constructor(address player_, address diamond_) {
         player = player_;
-        router = router_;
+        diamond = diamond_;
         for (uint8 i = 0; i < TOKENS_NAME.length; i++) {
-            tokens[i] = address(new Token(TOKENS_NAME[i], TOKENS_SYMBOL[i]));
+            tokens[i] = address(new Token(TOKENS_NAME[i], TOKENS_SYMBOL[i], address(this)));
             Token(tokens[i]).mint(player_, MAX);
             Token(tokens[i]).mint(address(this), 1);
-            Token(tokens[i]).approve(router_, 1e18);
+            Token(tokens[i]).approve(diamond_, 1e18);
         }
         factory = IFactory(msg.sender).deployFactory(player_);
-        IRouter(router_).addLiquidity(tokens[0], tokens[1], 1e18, 1e18, 1e18, 1e18, address(this), block.timestamp, factory);
+        IRouter(diamond_).addLiquidity(tokens[0], tokens[1], 1e18, 1e18, 1e18, 1e18, address(this), block.timestamp, factory);
     }
 
     function getPair() public returns(address pair) {
@@ -63,11 +54,14 @@ contract Level11Instance {
     }
 
     function swap(uint amountIn, uint amountOutMin, address[] calldata path) public returns(bool) {
+        IToken(tokens[1]).transferFrom(msg.sender, address(this), amountIn);
+        IToken(tokens[1]).approve(diamond, amountIn);
+
         // get actual quote
         uint256 prevQuote = getQuote();
 
         // do swap
-        IRouter(router).swapExactTokensForTokens(amountIn, amountOutMin, path, msg.sender, block.timestamp, factory);
+        IRouter(diamond).swapExactTokensForTokens(amountIn, amountOutMin, path, msg.sender, block.timestamp, factory);
 
         // get new quote
         uint256 currQuote = getQuote();
@@ -84,6 +78,6 @@ contract Level11Instance {
 
     function getQuote() public returns(uint256 quote) {
         (uint112 reserve0, uint112 reserve1, ) = IPair(getPair()).getReserves();
-        quote = IRouter(router).quote(1e18, reserve0, reserve1);
+        quote = IRouter(diamond).quote(1e18, reserve0, reserve1);
     }
 }
