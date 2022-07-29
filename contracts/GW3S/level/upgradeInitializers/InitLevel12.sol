40fbee77626ba032a3165e770520b00482a066ca
John <bioj@hotmail.fr>
2022-07-29 15:06:32 +0900

    GW3S contracts: fixing level 12

--- contracts/GW3S/level/upgradeInitializers/InitLevel12.sol
+++ contracts/GW3S/level/upgradeInitializers/InitLevel12.sol
@@ -11,6 +11,7 @@ pragma solidity ^0.8.15;
 
 import {LibAppStorage, AppStorage} from "../../libraries/LibAppStorage.sol";
 import {Level, Difficulty} from "../../libraries/LibLevel.sol";
+import "../../AMM/facets/FactoryFacet.sol";
 
 // It is expected that this contract is customized if you want to deploy your diamond
 // with data from a deployment script. Use the init function to initialize state variables
@@ -18,13 +19,18 @@ import {Level, Difficulty} from "../../libraries/LibLevel.sol";
 
 contract InitLevel12 {    
 
-    AppStorage internal s;
+    AppStorage internal _s;
 
     function init(address addr_) external {
-        s.level[12].addr = addr_;
-        s.level[12].id = 12;
-        s.level[12].title = "Arbitrage";
-        s.level[12].difficulty = Difficulty.MEDIUM;
+        _s.level[12].addr = addr_;
+        _s.level[12].id = 12;
+        _s.level[12].title = "Arbitrage";
+        _s.level[12].difficulty = Difficulty.MEDIUM;
+
+        address factory1 = FactoryFacet(address(this)).deployFactory(address(this));
+        address factory2 = FactoryFacet(address(this)).deployFactory(address(this));
+        _s.level_factories[12][0] = factory1;
+        _s.level_factories[12][1] = factory2;
         // More info here: https://eips.ethereum.org/EIPS/eip-2535#diamond-interface 
     }
 }
