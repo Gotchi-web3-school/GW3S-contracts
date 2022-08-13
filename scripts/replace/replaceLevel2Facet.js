b094a4031d429e8f91196aae252626c78a4513aa
John <bioj@hotmail.fr>
2022-08-13 18:27:35 +0900

    script: replace facets

---
+++ scripts/replace/replaceLevel2Facet.js
@@ -0,0 +1,79 @@
+/* global ethers */
+/* eslint prefer-const: "off" */
+
+const { readFile } = require("fs").promises
+const { getSelectors, FacetCutAction } = require('../libraries/diamond.js')
+const { deployed } = require("../libraries/deployed.js")
+const hardhat = require("hardhat")
+const FILE_PATH = './helpers/facetsContracts.json';
+
+async function replaceLevel2Facet () {
+  const accounts = await ethers.getSigners()
+  const contractOwner = accounts[0]
+
+  try {
+    contracts = JSON.parse(await readFile(FILE_PATH, "utf-8"))
+  } catch (e) {
+    console.log(e)
+  }
+
+  // deploy DiamondInit
+  // DiamondInit provides a function that is called when the diamond is upgraded to initialize state variables
+  // Read about how the diamondCut function works here: https://eips.ethereum.org/EIPS/eip-2535#addingreplacingremoving-functions
+  console.log("Deploying InitLevel2...")
+  const InitLevel2 = await ethers.getContractFactory('InitLevel2')
+  const initLevel2 = await InitLevel2.deploy()
+  await initLevel2.deployed()
+
+  deployed("InitLevel2", hardhat.network.name, initLevel2.address)
+  
+  // deploy facets
+  const FacetNames = [
+    'Level2Facet',
+  ]
+  const cut = []
+  for (const FacetName of FacetNames) {
+    console.log(`Deploying ${FacetName}...`)
+    const Facet = await ethers.getContractFactory(FacetName)
+    const facet = await Facet.deploy()
+    await facet.deployed()
+    
+    deployed(FacetName, hardhat.network.name, facet.address)
+
+    cut.push({
+      facetAddress: facet.address,
+      action: FacetCutAction.Replace,
+      functionSelectors: getSelectors(facet)
+    })
+  }
+
+  // upgrade diamond with facets
+  console.log('')
+  const diamondCut = await ethers.getContractAt('IDiamondCut', contracts.Diamond.mumbai.address)
+  let tx
+  let receipt
+  // call to init function
+  let functionCall = initLevel2.interface.encodeFunctionData('init', [cut[0].facetAddress])
+  console.log(functionCall)
+  tx = await diamondCut.diamondCut(cut, initLevel2.address, functionCall)
+  console.log('Diamond cut tx: ', tx.hash)
+  receipt = await tx.wait()
+  if (!receipt.status) {
+    throw Error(`Diamond upgrade failed: ${tx.hash}`)
+  }
+  console.log('Completed diamond cut')
+  return cut[0].facetAddress
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+if (require.main === module) {
+  replaceLevel2Facet()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error)
+      process.exit(1)
+    })
+}
+
+exports.replaceLevel2Facet = replaceLevel2Facet
---
+++ scripts/replace/replaceLevelLoupeFacet.js
@@ -0,0 +1,67 @@
+/* global ethers */
+/* eslint prefer-const: "off" */
+
+const { readFile } = require("fs").promises
+const { getSelector, FacetCutAction } = require('../libraries/diamond.js')
+const { deployed } = require("../libraries/deployed.js")
+const hardhat = require("hardhat")
+const FILE_PATH = './helpers/facetsContracts.json';
+
+async function deployLevelLoupeFacet () {
+
+  try {
+    contracts = JSON.parse(await readFile(FILE_PATH, "utf-8"))
+  } catch (e) {
+    console.log(e)
+  }
+
+  const cut = []
+  
+  // Deploying LevelLoupeFacet
+  //--------------------------------------------------------------
+  console.log(`Deploying LevelLoupeFacet...`)
+  const Facet = await ethers.getContractFactory("LevelLoupeFacet")
+  const facet = await Facet.deploy()
+  await facet.deployed()
+  
+  deployed("LevelLoupeFacet", hardhat.network.name, facet.address)
+  
+  cut.push({
+      facetAddress: facet.address,
+      action: FacetCutAction.Add,
+      functionSelectors: [
+        getSelector("function getFactoryLevel(uint256 levelId, uint8 pos) external view returns (address result)"),
+        getSelector("function getTokensLevel(uint256 levelId) external view returns (address[] memory result)"),
+    ]
+    })
+  //--------------------------------------------------------------
+
+
+  // upgrade diamond with facets
+  console.log('')
+  const diamondCut = await ethers.getContractAt('IDiamondCut', contracts.Diamond.mumbai.address)
+  let tx
+  let receipt
+  // call to init function
+  tx = await diamondCut.diamondCut(cut, "0x0000000000000000000000000000000000000000", [])
+  console.log('Diamond cut tx: ', tx.hash)
+  receipt = await tx.wait()
+  if (!receipt.status) {
+    throw Error(`Diamond upgrade failed: ${tx.hash}`)
+  }
+  console.log('Completed diamond cut')
+  return cut[0].facetAddress
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+if (require.main === module) {
+  deployLevelLoupeFacet()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error)
+      process.exit(1)
+    })
+}
+
+exports.deployLevelLoupeFacet = deployLevelLoupeFacet
