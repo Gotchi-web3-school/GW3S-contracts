34b390ba383b0f9679ff920e23029d9d3dcdef5a
John <bioj@hotmail.fr>
2022-08-17 04:09:13 +0900

    update the functions to add both front and back svg content

--- contracts/GW3S/Reward/Facets/SvgFacet.sol
+++ contracts/GW3S/Reward/Facets/SvgFacet.sol
@@ -7,7 +7,7 @@ import {LibSvg} from "../../libraries/LibSvg.sol";
 
 contract SvgFacet is Modifiers {
 
-    event RewardSvg(address indexed svg, uint256 levelId, uint256 _type);
+    event RewardSvg(LibSvg.Svg indexed svg, uint256 levelId, uint256 _type);
     /***********************************|
    |             Read Functions         |
    |__________________________________*/
@@ -15,16 +15,16 @@ contract SvgFacet is Modifiers {
     ///@notice Given an aavegotchi token id, return the combined SVG of its layers and its wearables
     ///@param _levelId the level identifier of the token to query
     ///@param _type the type of the level to query {LEVEL = 0, HIDDEN = 1, HACKER = 2}
-    ///@return ag_ The final svg which contains the combined SVG of its layers and its wearables
-    function getLevelRewardSvg(uint256 _levelId, uint256 _type) public view returns (string memory ag_) {
+    ///@return front & back: The final front and back content of the card
+    function getLevelRewardSvg(uint256 _levelId, uint256 _type) public view returns (string memory front, string memory back) {
         require(_type <= 2, "storeSvg: type does not exist");
         SvgStorage storage s = LibAppStorage.svgDiamondStorage();
-        require(s.svgLevelReward[_levelId][_type] != address(0), "SvgFacet: svg reward does not exist");
+        require(s.svgLevelReward[_levelId][_type].front != address(0), "SvgFacet: svg reward does not exist");
 
-        bytes memory svg;
-        svg = LibSvg.getSvg(_levelId, _type);
+        (bytes memory front_, bytes memory back_) = LibSvg.getSvg(_levelId, _type);
 
-        ag_ = string(abi.encodePacked('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">', svg, "</svg>"));
+        front = string(abi.encodePacked(front_));
+        back = string(abi.encodePacked(back_));
     }
 
     /***********************************|
@@ -35,27 +35,27 @@ contract SvgFacet is Modifiers {
     ///@param _svg the new svg string
     ///@param levelId the level identifier of the token to query
     ///@param _type the type of the level to query {LEVEL = 0, HIDDEN = 1, HACKER = 2}
-    function storeSvg(string calldata _svg, uint256 levelId, uint256 _type) external onlyOwner returns(address){
+    function storeSvg(string[] calldata _svg, uint256 levelId, uint256 _type) external onlyOwner returns(LibSvg.Svg memory contracts){
         SvgStorage storage s = LibAppStorage.svgDiamondStorage();
         require(_type <= 2, "storeSvg: type does not exist");
-        require(s.svgLevelReward[levelId][_type] == address(0), "SvgFacet: svg already existing");
+        require(s.svgLevelReward[levelId][_type].front == address(0), "SvgFacet: svg already existing");
 
-        address contract_ = address(LibSvg.storeSvg(_svg, levelId, _type));
-        emit RewardSvg(contract_, levelId, _type);
+        contracts = LibSvg.storeSvg(_svg, levelId, _type);
+        emit RewardSvg(contracts, levelId, _type);
 
-        return contract_;
+        return contracts;
     }
 
     ///@notice Allow an item manager to store a new  svg
     ///@param _svg the new svg string
     ///@param levelId the level identifier of the token to query
     ///@param _type the type of the level to query {LEVEL = 0, HIDDEN = 1, HACKER = 2}
-    function updateSvg(string calldata _svg, uint256 levelId, uint256 _type) external onlyOwner returns(address) {
+    function updateSvg(string[] calldata _svg, uint256 levelId, uint256 _type) external onlyOwner returns(LibSvg.Svg memory contracts) {
         require(_type <= 2, "storeSvg: type does not exist");
 
-        address contract_ = address(LibSvg.storeSvg(_svg, levelId, _type));
-        emit RewardSvg(contract_, levelId, _type);
+        contracts = LibSvg.storeSvg(_svg, levelId, _type);
+        emit RewardSvg(contracts, levelId, _type);
         
-        return contract_;
+        return contracts;
     }
 }
